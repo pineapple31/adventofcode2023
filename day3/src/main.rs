f3f41d53083db99395b8c631ac1b5f3344110157
William Lefèvre <william.lefevre@balmo.eei.cena.fr>
2023-12-06 17:44:00 +0100

    day3 in progress

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,12 +1,21 @@
+use std::{fs::File, io::{BufReader, BufRead}};
 
 fn part1() {
     println!("---- Part 1 ----");
-    let file_path = "./input.txt";
-    let binding = read_to_string(file_path).unwrap();
-    let content : Vec<&str>= binding
-        .as_str()
-        .lines()
-        .collect();
+    let file = BufReader::new(File::open("./input.txt").expect("Unable to open file"));
+    let mut matrice: Vec<Vec<char>> = Vec::new();
+    let mut line_matrice: Vec<char> = Vec::new();
+    for line in file.lines() {
+        for ch in line.expect("Unable to read line").chars() {
+            line_matrice.push(ch);
+            print!("{ch}");
+        }
+        print!("\n");
+        matrice.push(line_matrice.clone());
+        line_matrice.clear();
+    }
+    println!("{:?}",matrice[14][2]);
+    println!("----------------");
 }
 
 fn main() {
